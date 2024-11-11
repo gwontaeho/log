fbfbae81c83fbfa745bf6a37116d7403317a7a04
gwontaeho <taeho8199@gmail.com>
2024-11-11 17:29:46 +0300

    ;

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/jenkins/:path*",
+        destination: "http://jenkins.singlewindow.info:30143/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
