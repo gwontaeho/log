d62526f31269f7419e46d527950b15a307bf00ed
gwontaeho <taeho8199@gmail.com>
2024-11-11 20:08:08 +0300

    ;

--- app/page.tsx
+++ app/page.tsx
@@ -38,14 +38,15 @@ export default function Home() {
   const handleSubmit = (event: any) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const textarea = formData.get("textarea");
-    const extracted = extractResolvedFiles(textarea);
+    const name = (formData.get("name") || "Resolved") as string;
+    const log = formData.get("log");
+    const extracted = extractResolvedFiles(log);
 
     const worksheet = XLSX.utils.aoa_to_sheet(extracted);
     const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Logs");
-    worksheet["!cols"] = [{ wch: 20 }, { wch: 30 }, { wch: 20 }];
-    XLSX.writeFile(workbook, "Logs.xlsx", { compression: true });
+    XLSX.utils.book_append_sheet(workbook, worksheet, name);
+    worksheet["!cols"] = [{ wch: 30 }, { wch: 30 }, { wch: 30 }];
+    XLSX.writeFile(workbook, `${name}.xlsx`, { compression: true });
   };
 
   return (
@@ -53,13 +54,22 @@ export default function Home() {
       className="p-8 flex flex-col items-center gap-8"
       onSubmit={handleSubmit}
     >
-      <span>Paste here 👇</span>
+      <span>File name 👇</span>
+      <input
+        name="name"
+        className="h-10 border p-1 outline-none focus:border-green-600 hover:border-green-600"
+        placeholder="Resolved"
+      />
+      <span>Paste log here 👇</span>
       <textarea
-        name="textarea"
-        className="p-1 h-[500px] w-full border outline-none"
+        name="log"
+        spellCheck="false"
+        className="text-sm p-1 h-[500px] w-full border outline-none focus:border-green-600 hover:border-green-600"
         placeholder="log..."
       />
-      <button className="border h-10 w-20">Export</button>
+      <button className="border h-10 w-20 hover:border-green-600">
+        Export
+      </button>
     </form>
   );
 }
