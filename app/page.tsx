329ba5326fb33e315f0782b510e10ed851d579be
gwontaeho <taeho8199@gmail.com>
2024-11-11 18:19:22 +0300

    ;

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,63 @@
 "use client";
 import { useEffect } from "react";
-import axios from "axios";
+import * as XLSX from "xlsx";
 
 export default function Home() {
-  useEffect(() => {
-    getLog();
-  }, []);
+  useEffect(() => {}, []);
 
-  const getLog = async () => {
-    // const JENKINS_URL = `jenkins.singlewindow.info:30143`;
+  const extractResolvedFiles = (log: any) => {
+    const startMarker = "The following files have been resolved:";
+    const endMarker = "BUILD SUCCESS";
 
-    const url = `/jenkins/job/tancis-batch-com/179/consoleText`;
-    try {
-      const response = await axios.get(url);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
+    const logLines = log.split("\n");
+
+    const startIndex = logLines.findIndex((line: any) =>
+      line.includes(startMarker)
+    );
+    const endIndex =
+      logLines.findIndex((line: any) => line.includes(endMarker)) - 2;
+
+    if (startIndex !== -1 && endIndex !== -1 && startIndex < endIndex) {
+      return logLines
+        .slice(startIndex + 1, endIndex)
+        .filter((line: any) => line.trim() !== "")
+        .map((line: string) => {
+          let ary: any = [];
+          const [time, data] = line.split(/\s*\[.*?\]\s*/);
+          ary.push(time);
+          for (let i = 0; i < data.length; i++) {
+            ary.push(data.split(":")[i]);
+          }
+          return ary;
+        });
+    } else {
+      return [];
     }
   };
-  return <div></div>;
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const textarea = formData.get("textarea");
+    const extracted = extractResolvedFiles(textarea);
+
+    const worksheet = XLSX.utils.aoa_to_sheet(extracted);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Logs");
+    worksheet["!cols"] = [{ wch: 10 }, { wch: 30 }, { wch: 20 }];
+    XLSX.writeFile(workbook, "Logs.xlsx", { compression: true });
+  };
+
+  return (
+    <form
+      className="p-8 flex flex-col items-center gap-8"
+      onSubmit={handleSubmit}
+    >
+      <textarea
+        name="textarea"
+        className="p-1 h-[500px] w-full border outline-none"
+      />
+      <button className="border h-10 w-20">Go</button>
+    </form>
+  );
 }
