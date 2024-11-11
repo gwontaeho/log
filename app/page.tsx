6bbfed4649a580a6634a4a8cc4110539e2cd3d30
gwontaeho <taeho8199@gmail.com>
2024-11-11 19:49:08 +0300

    ;

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
     const worksheet = XLSX.utils.aoa_to_sheet(extracted);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Logs");
-    worksheet["!cols"] = [{ wch: 10 }, { wch: 30 }, { wch: 20 }];
+    worksheet["!cols"] = [{ wch: 20 }, { wch: 30 }, { wch: 20 }];
     XLSX.writeFile(workbook, "Logs.xlsx", { compression: true });
   };
 
@@ -53,11 +53,13 @@ export default function Home() {
       className="p-8 flex flex-col items-center gap-8"
       onSubmit={handleSubmit}
     >
+      <span>Paste here 👇</span>
       <textarea
         name="textarea"
         className="p-1 h-[500px] w-full border outline-none"
+        placeholder="log..."
       />
-      <button className="border h-10 w-20">Go</button>
+      <button className="border h-10 w-20">Export</button>
     </form>
   );
 }
